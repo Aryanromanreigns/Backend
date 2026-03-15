750383fdaccca7c412a1b175fa94fce2f71d9672
Aryan Kumar <aryanempire168@gmail.com>
2026-03-15 16:11:11 +0530

    Add files via upload

---
+++ 02_npm/script.js
@@ -0,0 +1,18 @@
+//npm understanding
+//node js mein jo installed aata hai wo node module hai
+//npm se jo download krte hai wo package hai
+
+
+// install
+//npm i package_name
+
+
+
+//uninstall
+//npm uninstall packagae_name
+
+
+
+//dependicies-package and packgae ke dependies
+//devdependicies-those package jo sirf development main kaam aate hai
+
