96fa29343d6ce1b677264b9d1f8f08819e555266
Aryan Kumar <aryanempire168@gmail.com>
2026-03-22 20:52:37 +0530

    Add files via upload

---
+++ 08_ejs_and_crud_operation/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const usermodel = require('./models/user');
+
+
+app.set('view engine','ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.get('/',function(req,res){
+    res.render('index');
+});
+
+app.get('/read',async function(req,res){
+    let users = await usermodel.find();
+
+    res.render('read',{users});
+});
+app.get('/delete/:id',async function(req,res){
+    let users = await usermodel.findOneAndDelete({_id:req.params.id});
+
+    res.redirect('/read');
+});
+
+app.post('/create',async function(req,res){
+    let {name ,email ,image} = req.body;
+    let createuser = await usermodel.create({
+        name,
+        email,
+        image
+    });
+    res.redirect('/read');
+    
+});
+
+
+
+app.listen(3000,function(err){
+    console.log(`port is running of 3000`);
+});
+
